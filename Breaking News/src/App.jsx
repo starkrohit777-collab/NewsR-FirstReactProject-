48b4acce6a257aae045326d1cc7fbe466dde3c8b
starkrohit777-collab <starkrohit777@gmail.com>
2026-08-10 18:27:27 +0530

    Fix Home component path

--- Breaking News/src/App.jsx
+++ Breaking News/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "./Components/Navbar";
-import Home from "./Components/Pages/Home";
+import Home from "./Components/Pages/home";
 
 function App() {
   const [category, setCategory] = useState("general");
